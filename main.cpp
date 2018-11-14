f6379009014f1e0f535b4f1992d8747801735ab2
vincatzero <vincatzero@gmail.com>
2018-11-13 19:09:48 -0600

    Assignment 5 Testing

---
+++ main.cpp
@@ -0,0 +1,22 @@
+/*	Project:    NAME OF PROJECT GOES HERE
+	Name:       YOUR NAME GOES HERE
+    Partner:    PARTNER'S NAME IF ANY
+    Class:      example: CSC 1810 - Section 1
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	cout << "Hello, world." << endl;
+
+	// This causes the program to pause at its completion.
+	{
+		char c;
+		cout << "Hit enter to exit:";
+		cin.get(c);
+	}
+	return 0;
+}
