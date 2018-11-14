d50283f166a41be4a069425089da6f58809d072e
vincatzero <vincatzero@gmail.com>
2018-11-14 10:49:06 -0600

    test

--- main.cpp
+++ main.cpp
@@ -10,7 +10,34 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-	cout << "Hello, world." << endl;
+	string UserOption;
+	while (UserOption != "6")
+	{
+		cout << "        * MENU *" << endl
+			 << endl
+
+			 << "1 - Create Chromosome" << endl
+			 << "2 - Analyze Chromosome" << endl
+			 << "3 - Output Chromosome to File" << endl
+			 << "4 - Input Chromosome from File" << endl
+			 << "5 - Combine Chromosomes" << endl
+			 << "6 - Exit" << endl
+			 << endl
+			 << "Please Enter your Choice (1-6) :";
+		getline(cin, UserOption);
+		while ((UserOption > "6") || (UserOption < "1") || (UserOption.length() > 1))
+		{
+			cout << "Please enter a valid choice: ";
+			getline(cin, UserOption);
+		}
+	}
+	int userChoice = stoi(UserOption);
+	switch (userChoice)
+	{
+	case 6:
+		cout << "so long fuckers";
+		break;
+	}
 
 	// This causes the program to pause at its completion.
 	{
@@ -22,6 +49,5 @@ int main(int argc, char *argv[])
 }
 
 //testing testing
-
 
 //check this out
